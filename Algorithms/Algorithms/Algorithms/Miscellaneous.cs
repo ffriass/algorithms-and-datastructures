bd06f7cef34c6985408bd2f6a82e94c804caea2a
Franklin Antonio Frias Vallejo <fcfrias33@gmail.com>
2021-06-06 00:16:47 -0400

    Add some algorithms using loop vs linq

---
+++ Algorithms/Algorithms/Algorithms/Miscellaneous.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms.Algorithms
+{
+    public class Miscellaneous
+    {
+        /// <summary>
+        /// Given a text, return the words that repeates more than the specified in the parameter using loop.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="overRepetitionCount"></param>
+        /// <returns><see cref="List{T}>"/></returns>
+        public object CountWordsRepetitionLoop(string text, int overRepetitionCount = 1)
+        {
+            string cleanedText = text.Replace(",", "");
+            var words = cleanedText.Split(" ");
+            Dictionary<string, int> dictionaryWords = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                if (dictionaryWords.ContainsKey(word))
+                    dictionaryWords[word] = dictionaryWords[word] + 1;
+                else
+                    dictionaryWords.Add(word, 1);
+            }
+
+            var duplicatedWords = dictionaryWords
+                .Where(a => a.Value > 1)
+                .Select(x => new { Word = x.Key, Count = x.Value })
+                .ToList();
+            return duplicatedWords;
+        }
+
+        /// <summary>
+        /// Given a text, return the words that repeates more than the specified in the parameter without using loop.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="overRepetitionCount"></param>
+        /// <returns><see cref="List{T}>"/></returns>
+        public object CountWordsRepetition(string text, int overRepetitionCount = 0)
+        {
+            string cleanedText = text.Replace(",", "");
+            var words = cleanedText.Split(" ");
+
+            //To return an anonymous list
+            var query = words.GroupBy(word => word)
+              .Where(g => g.Count() > overRepetitionCount)
+              .Select(x => new { Word = x.Key, Count = x.Count() })
+              .ToList();
+
+            //To return a Dictionary
+            var query2 = words.GroupBy(word => word)
+              .Where(g => g.Count() > overRepetitionCount)
+              .ToDictionary(x => x.Key, y => y.Count());
+            return query;
+        }
+
+        /// <summary>
+        /// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct. 
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns><see cref="bool"/></returns>
+        public bool CheckDuplicatedValues(int[] nums)
+        {
+            bool areDuplicated = nums.GroupBy(n => n).Any(x => x.Count() > 1);
+            return areDuplicated;
+        }
+
+        /// <summary>
+        /// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+        /// Using Loops
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns><see cref="bool"/></returns>
+        public bool CheckDuplicatedValuesLoop(int[] nums)
+        {
+            Dictionary<int, int> recordedNums = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int currentNum = nums[i];
+                if (recordedNums.ContainsKey(currentNum))
+                    return true;
+                recordedNums.Add(currentNum,1);
+            }
+            return false;
+        }
+    }
+}
--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Algorithms.Algorithms;
 using Algorithms.BinaryTree;
 
 namespace Algorithms
@@ -7,6 +8,21 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
+            string text = $"The two castaways, John Ferrier and the little girl who had shared his fortunes and had been adopted as his daughter, accompanied the Mormons to the end of their great pilgrimage. Little Lucy Ferrier was borne along pleasantly enough in Elder Stangerson’s waggon, a retreat which she shared with the Mormon’s three wives and with his son, a headstrong forward boy of twelve. Having rallied, with the elasticity of childhood, from the shock caused by her mother’s death, she soon became a pet with the women, and reconciled herself to this new life in her moving canvas-covered home. In the meantime Ferrier having recovered from his privations, distinguished himself as a useful guide and an indefatigable hunter. So rapidly did he gain the esteem of his new companions, that when they reached the end of their wanderings, it was unanimously agreed that he should be provided with as large and as fertile a tract of land as any of the settlers, with the exception of Young himself, and of Stangerson, Kemball, Johnston, and";
+
+            Miscellaneous miscellaneous = new Miscellaneous();
+            //var result1 = miscellaneous.CountWordsRepetitionLoop(text, 1);
+            //var result2 = miscellaneous.CountWordsRepetition(text,1);
+
+            int[] nums = { 1, 3, 4, 2, 1 };
+            int[] nums2 = { 1, 3, 4, 2, 5 };
+
+            var result3 = miscellaneous.CheckDuplicatedValues(nums);
+            var result4 = miscellaneous.CheckDuplicatedValues(nums2);
+
+            var result5 = miscellaneous.CheckDuplicatedValuesLoop(nums);
+            var result6 = miscellaneous.CheckDuplicatedValuesLoop(nums2);
+
 
             //Using the Tree structure
             Tree tree = new Tree();
